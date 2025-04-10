5fde280290cd5df6e3e601960d85611ea0e9b5cb
coelho-glith <coelho.soldado7@gmail.com>
2025-04-10 13:59:48 -0300

    Add project files.

---
+++ ClubeDaLeitura-2025.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubeDaLeitura_2025.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
